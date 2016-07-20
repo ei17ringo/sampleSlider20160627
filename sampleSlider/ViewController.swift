f431adfd0ba39ef67eab85a30fe8fe4dee164215
Eriko Ichinohe <ei17ringo@gmail.com>
2016-07-20 16:14:49 +0800

    init commit

--- sampleSlider/ViewController.swift
+++ sampleSlider/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mySlider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func changeSlider(sender: UISlider) {
+        print(sender.value)
+        //TODO: sender.valueが0.5以下の時、「音が小さいです」0.5より大きい時「音が大きいです」とデバッグエリアに表示せよ（ヒント：if文を使用）
+        if sender.value <= 0.5 {
+            print("音が小さいです")
+        }else{
+            print("音が大きいです")
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
